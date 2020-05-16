1627d606c23edd36e974e4655ee537221bbc8787
Christian Lengert <christian.lengert@tuta.io>
2020-05-16 11:19:24 +0200

    Update piecash.py
    Use os.path.expanduser

--- piecash.py
+++ piecash.py
@@ -7,13 +7,13 @@ import csv
 import xml.etree.ElementTree
 # from xml.etree import ElementTree
 
-GNUCASH_DIR = '/home/me/Nextcloud/GnuCash/*.gnca'
+GNUCASH_DIR = os.path.expanduser('~/Nextcloud/GnuCash/*.gnca')
 list_of_files = glob.glob(GNUCASH_DIR) # * means all if need specific format then *.csv
 latest_file = max(list_of_files, key=os.path.getctime)
 
 context = xml.etree.ElementTree.iterparse(latest_file, events=("start", "end"))
 
-for event,elem in context:
+for event, elem in context:
     tag = elem.tag
     if tag == '{http://www.gnucash.org/XML/gnc}account':
         print(tag)
